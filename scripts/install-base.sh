2583742631e6c3c2615bb26efa6ff50cc347ac81
Loic Lambiel <llambiel@own3d.ch>
2020-04-29 00:35:31 +0200

    WIP

--- scripts/install-base.sh
+++ scripts/install-base.sh
@@ -3,22 +3,19 @@
 # stop on errors
 set -eu
 
-if [[ $PACKER_BUILDER_TYPE == "qemu" ]]; then
-	DISK='/dev/vda'
-else
-	DISK='/dev/sda'
-fi
-
-FQDN='vagrant-arch.vagrantup.com'
+DISK='/dev/vda'
+FQDN='amnesiac'
 KEYMAP='us'
 LANGUAGE='en_US.UTF-8'
-PASSWORD=$(/usr/bin/openssl passwd -crypt 'vagrant')
+PASSWORD=$(/usr/bin/openssl passwd -crypt 'arch')
 TIMEZONE='UTC'
 
 CONFIG_SCRIPT='/usr/local/bin/arch-config.sh'
-ROOT_PARTITION="${DISK}1"
+BOOT_PARTITION="${DISK}1"
+ROOT_PARTITION="${DISK}2"
+BOOT_DIR='/mnt/boot'
 TARGET_DIR='/mnt'
-COUNTRY=${COUNTRY:-US}
+COUNTRY=${COUNTRY:-CH}
 MIRRORLIST="https://www.archlinux.org/mirrorlist/?country=${COUNTRY}&protocol=http&protocol=https&ip_version=4&use_mirror_status=on"
 
 echo "==> Setting local mirror"
@@ -31,24 +28,41 @@ echo "==> Destroying magic strings and signatures on ${DISK}"
 /usr/bin/dd if=/dev/zero of=${DISK} bs=512 count=2048
 /usr/bin/wipefs --all ${DISK}
 
-echo "==> Creating /root partition on ${DISK}"
-/usr/bin/sgdisk --new=1:0:0 ${DISK}
+echo "==> Creating /boot partition on ${DISK}"
+/usr/bin/sgdisk --new=1:2M:128M ${DISK} -t 0:EF00
 
-echo "==> Setting ${DISK} bootable"
-/usr/bin/sgdisk ${DISK} --attributes=1:set:2
+echo "==> Creating /root partition on ${DISK}"
+/usr/bin/sgdisk --new=2:0:0 ${DISK} -t 0:8300
+
+echo '==> Creating /boot filesystem (FAT32)'
+/usr/bin/mkfs.vfat -F32 -n EFI ${BOOT_PARTITION}
 
 echo '==> Creating /root filesystem (ext4)'
 /usr/bin/mkfs.ext4 -O ^64bit -F -m 0 -q -L root ${ROOT_PARTITION}
+
+echo "==> Creating ${BOOT_DIR}"
+/usr/bin/mkdir ${BOOT_DIR}
+
+echo "==> Mounting ${BOOT_PARTITION} to ${BOOT_DIR}"
+/usr/bin/mount ${BOOT_PARTITION} ${BOOT_DIR}
 
 echo "==> Mounting ${ROOT_PARTITION} to ${TARGET_DIR}"
 /usr/bin/mount -o noatime,errors=remount-ro ${ROOT_PARTITION} ${TARGET_DIR}
 
 echo '==> Bootstrapping the base installation'
 /usr/bin/pacstrap ${TARGET_DIR} base base-devel
-/usr/bin/arch-chroot ${TARGET_DIR} pacman -S --noconfirm gptfdisk openssh syslinux
-/usr/bin/arch-chroot ${TARGET_DIR} syslinux-install_update -i -a -m
-/usr/bin/sed -i "s|sda3|${ROOT_PARTITION##/dev/}|" "${TARGET_DIR}/boot/syslinux/syslinux.cfg"
-/usr/bin/sed -i 's/TIMEOUT 50/TIMEOUT 10/' "${TARGET_DIR}/boot/syslinux/syslinux.cfg"
+/usr/bin/arch-chroot ${TARGET_DIR} pacman -S --noconfirm linux openssh cloud-utils
+echo '==> Installing boot loader'
+/usr/bin/arch-chroot ${TARGET_DIR} /usr/bin/bootctl --path=/boot install
+
+echo '==> Creating boot loader entry'
+/usr/bin/mkdir -p ${BOOT_DIR}/loader/entries/
+cat <<-EOF > "${BOOT_DIR}/loader/entries/arch.conf
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /initramfs-linux.img
+options root=/dev/vda2 rw
+EOF
 
 echo '==> Generating the filesystem table'
 /usr/bin/genfstab -p ${TARGET_DIR} >> "${TARGET_DIR}/etc/fstab"
@@ -69,23 +83,12 @@ cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
 	/usr/bin/ln -s '/usr/lib/systemd/system/dhcpcd@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service'
 	/usr/bin/sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
 	/usr/bin/systemctl enable sshd.service
+        /usr/bin/systemctl enable systemd-timesyncd.service
 
 	# Workaround for https://bugs.archlinux.org/task/58355 which prevents sshd to accept connections after reboot
 	/usr/bin/pacman -S --noconfirm rng-tools
 	/usr/bin/systemctl enable rngd
 
-	# Vagrant-specific configuration
-	/usr/bin/useradd --password ${PASSWORD} --comment 'Vagrant User' --create-home --user-group vagrant
-	echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_vagrant
-	echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant
-	/usr/bin/chmod 0440 /etc/sudoers.d/10_vagrant
-	/usr/bin/install --directory --owner=vagrant --group=vagrant --mode=0700 /home/vagrant/.ssh
-	/usr/bin/curl --output /home/vagrant/.ssh/authorized_keys --location https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
-	/usr/bin/chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
-	/usr/bin/chmod 0600 /home/vagrant/.ssh/authorized_keys
-
-	# clean up
-	/usr/bin/pacman -Rcns --noconfirm gptfdisk
 EOF
 
 echo '==> Entering chroot and configuring system'
@@ -98,5 +101,12 @@ echo '==> Adding workaround for shutdown race condition'
 
 echo '==> Installation complete!'
 /usr/bin/sleep 3
+/usr/bin/umount ${BOOT_DIR}
 /usr/bin/umount ${TARGET_DIR}
-/usr/bin/systemctl reboot
+# Turning network interfaces down to make sure SSH session was dropped on host.
+# More info at: https://www.packer.io/docs/provisioners/shell.html, section - Handling reboots
+echo '==> Turning down network interfaces and rebooting'
+for i in $(/usr/bin/netstat -i | /usr/bin/tail +3 | /usr/bin/awk '{print $1}'); do /usr/bin/ip link set ${i} down; done
+echo '==> Pacman cleanup'
+/usr/bin/yes | /usr/bin/pacman -Scc
+/usr/bin/systemctl shutdown
--- srv/enable-ssh.sh
+++ srv/enable-ssh.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-PASSWORD=$(/usr/bin/openssl passwd -crypt 'vagrant')
+PASSWORD=$(/usr/bin/openssl passwd -crypt 'arch')
 
 echo "==> Enabling SSH"
 # Vagrant-specific configuration
-/usr/bin/useradd --password ${PASSWORD} --comment 'Vagrant User' --create-home --user-group vagrant
-echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_vagrant
-echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant
-/usr/bin/chmod 0440 /etc/sudoers.d/10_vagrant
+/usr/bin/useradd --password ${PASSWORD} --comment 'Arch User' --create-home --user-group arch
+echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_archuser
+echo 'arch ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_archuser
+/usr/bin/chmod 0440 /etc/sudoers.d/10_archuser
 /usr/bin/systemctl start sshd.service
